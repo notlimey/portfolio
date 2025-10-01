caf5db9ae428e02498407584514f3453c2e80609
Martin Myhre <70258183+notlimey@users.noreply.github.com>
2025-10-01 21:30:05 +0200

    health

--- apps/web/app/api/health/route.ts
+++ apps/web/app/api/health/route.ts
@@ -1,5 +1,10 @@
 import {NextResponse} from "next/server";
 
-export async function GET() {
+export async function GET(req: Request) {
+    const authKey = req.headers.get('X-Monitoring-Key');
+    if (authKey !== "cQsAuhcLVZKwZLeEdJXSrgK7IgT7o5v1PwzErxbe55s=") {
+        return NextResponse.json({ status: "OK" }, { status: 403 })
+    }
+
     return NextResponse.json({ status: 'OK' }, { status: 200 });
 }
